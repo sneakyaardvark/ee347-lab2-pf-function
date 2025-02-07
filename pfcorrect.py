6158470056697d54368498f762e9894780154815
Sneaky Aardvark <81195290+sneakyaardvark@users.noreply.github.com>
2025-02-06 20:46:21 -0800

    rewrite out of frustration

--- pfcorrect.py
+++ pfcorrect.py
@@ -1,35 +1,8 @@
 from cmath import rect as __cmath_rect
+from typing import Any
 import numpy as np
 import numpy.typing as npt
 from math import acos
-
-def rect(s_load: npt.ArrayLike, pf: float) -> npt.NDArray[np.complex64] | complex:
-    '''
-    Compute the rectangular form given magnitude(s) and an angle.
-    Similar to cmath.rect, but can operate on array types.
-    
-    Parameters
-    ----------
-    s_load : array_like
-        the real power
-    pf : float
-        the target power factor
-    
-    Returns
-    -------
-    rect : ndarray or scalar
-        the rectangular form, a+bj
-
-    See Also
-    --------
-    cmath.rect :
-        Python cmath module version of this function for scalar inputs
-    '''
-    def v_func(a: complex, pf: float) -> float:
-        # virtual function to vectorize with numpy for applying to every element in the array
-        return __cmath_rect(a.real / pf, acos(pf)).imag
-    return np.vectorize(v_func)(s_load, pf)
-
 
 def round_step(a: npt.NDArray[np.float64] | float, step: float) -> npt.NDArray[np.float64] | float:
     '''
@@ -37,30 +10,55 @@ def round_step(a: npt.NDArray[np.float64] | float, step: float) -> npt.NDArray[n
 
     Parameters
     ----------
+    a : ndarray or float
+        the array to round
+    step : float
+        the step to round to
+
+    Returns
+    -------
+    b : ndarray or float
+        a, rounded to step
 
     '''
     return np.round(a / step) * step
 
 
-def correct(s_load: npt.ArrayLike, s_min: complex = 1+0j, s_max: complex = 10+10j) -> npt.NDArray[np.float64] | float:
+def correct(s_load: npt.ArrayLike) -> npt.NDArray[np.float64] | float:
     '''
-    Compute power factor correction, Q_c
+    Compute power factor correction, Q_c for a minumum power factor of 0.85 and maximum of 1.0
     
     Parameters
     ----------
-    s_load : array_like
-        the real power
-    s_min : complex
-        the minimum allowed s_load
+    s_load : complex array_like
+        the real power, P + jQ
 
     Returns
-    ------
-        Q_c : ndarray or scalar
-            the corrected Q value
+    -------
+    Q_c : ndarray or scalar
+            the corrective Q value
     '''
-    pf_target = 0.85
-    if (np.max(s_load).real > s_max.real or np.min(s_load).real < s_min.real 
-        or np.max(s_load).imag > s_max.imag or np.min(s_load).imag < s_min.imag):
-        print("invalid s_load")
-        return np.empty_like(s_load)
-    return np_rect(s_load, pf_target)
+    pf_min = 0.85
+    pf_max = 1.0
+
+    def v_func(s: complex) -> float:
+        q_new = (s.real / pf_min) * np.sin(np.acos(pf_min))
+        q_c = q_new - s.imag
+        q_c = round_step(q_c, 0.5)
+        q_new = q_c + s.imag
+
+        # check if rounding created PF < 0.85 or > 1.0
+        s_new = np.sqrt(s.real**2 + q_new**2)
+        pf_new = s.real / s_new
+
+        if pf_new < pf_min:
+            return q_c - 0.5
+        elif pf_new > pf_max:
+            # no compensation was req'd
+            return s.imag
+        return q_c
+
+    return np.vectorize(v_func)(s_load)
+
+if __name__ == "__main__":
+    print(correct(1.5+6j))
--- plot.py
+++ plot.py
@@ -1,34 +1,72 @@
 import numpy as np
 from matplotlib import pyplot as plt
+from matplotlib.cm import Oranges, Spectral
 from pfcorrect import correct
-import csv
 
-PF_COL = 3
-S_COL = 4
-Q_COL = 5
 
 def plot():
-    fig = plt.figure(figsize=(8, 3))
+    # set up plotting
+
+    fig = plt.figure(figsize=(8, 8))
     ax1 = fig.add_subplot(121, projection='3d')
     ax2 = fig.add_subplot(122, projection='3d')
 
-    _p_load = []
-    _q_load = []
-    _pf = []
-    with open("data/data_all.csv", newline='') as csvf:
-        rdr = csv.reader(csvf)
-        for row in rdr:
-            print(row)
-            _pf.append(float(row[PF_COL]))
-            _p_load.append(float(row[S_COL]))
-            _q_load.append(float((row[Q_COL])))
+    xyticks = np.arange(0, 11, 2)
+    zticks = np.arange(0, 1.1, 0.1)
 
-    ax1.set(title='Before Compensation', xlabel='$P_{load}$', ylabel='$Q_{load}$', zlabel='PF')
-    ax1.plot(_p_load, _q_load, _pf)
+    _p_load = np.linspace(1, 10)
+    _q_load = np.copy(_p_load)
+    _p_load, _q_load = np.meshgrid(_p_load, _q_load)
+    _pf = np.divide(_p_load, np.sqrt(np.add(np.square(_p_load),np.square(_q_load))))
 
-    ax2.set(title='After Compensation', xlabel='$P_{load}$', ylabel='$Q_{load}$', zlabel='PF')
 
+    # plot
+    _ = ax1.set(
+            title='Before Compensation',
+            xlabel='$P_{load}$ (MW)',
+            xlim=(0,11),
+            xticks=xyticks,
+            ylabel='$Q_{load}$ (MVAR)',
+            ylim=(0,11),
+            yticks=xyticks,
+            zlabel='PF',
+            zlim=(0, 1.0),
+            zticks=zticks
+            )
+    surf_before = ax1.plot_surface(_p_load, _q_load, _pf, cmap=Spectral, linewidth=0, antialiased=False, rcount=200, ccount=200)
+
+    # adjust default viewing angle
+    ax1.view_init(30, 220)
+    fig.colorbar(surf_before, location='bottom', ticks=[0, 0.25, 0.5, 0.75, 1.0])
+
+    
+    _p_load = np.linspace(1, 10)
+    _q_load = np.copy(_p_load)
+    _p_load, _q_load = np.meshgrid(_p_load, _q_load)
+    _s_load = _p_load + 1j * _q_load
+    _q_c = correct(_s_load)
+    _q_new = _q_load + _q_c
+
+    _pf = np.divide(_p_load, np.sqrt(np.add(np.square(_p_load), np.square(_q_new))))
+
+    # plot
+    _ = ax2.set(
+            title='After Compensation',
+            xlabel='$P_{load}$ (MW)',
+            xticks=xyticks,
+            xlim=(0,11),
+            ylabel='$Q_{load}$ (MVAR)',
+            yticks=xyticks,
+            ylim=(0,11),
+            zlabel='PF (corrected)',
+            zlim=(0, 1.0),
+            zticks=zticks
+            )
+    surf_after = ax2.plot_surface(_p_load, _q_load, _pf, cmap=Spectral, linewidth=0)
+    # adjust default viewing angle
+    ax2.view_init(30, 220)
+    fig.colorbar(surf_after, location='bottom', ticks=[0.80, 0.85, 0.90, 0.95, 1.0])
     plt.show()
-            
+
 if __name__ == "__main__":
     plot()
